148dcf08d0cedc03244f95138d0aaeca3481edc9
Sulley Abdul Latif <sulleyabdullatif404@gmail.com>
2024-12-10 23:02:13 +0000

    ggv

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 					name='description'
 					content='TechVerge Africa is a startup focused on empowering African youth through accessible technology education. We aim to bridge the gap between talent and opportunity, helping young minds develop skills, build tech solutions, and drive positive change.'
 				/>
-				<link rel='icon' href='/favicon1.ico' />
+				<link rel='icon' href='/favicon3.ico' />
 			</Head>
 
 			<HomePageComponent />
